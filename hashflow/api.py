a6d6dcad2808d4dd6686d4249daf50d289c52535
Sameer Brenn <sameer@hashflow.com>
2023-05-09 10:01:46 -0600

    clean

--- hashflow/api.py
+++ hashflow/api.py
@@ -111,30 +111,15 @@ class HashflowApi:
         r.raise_for_status()
         return r.json()
 
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in 
-    this function because it is programmed to be pretty 
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
 
 if __name__ == "__main__":
     api = HashflowApi(
         os.environ["HASHFLOW_AUTHORIZATION_KEY"], "qa", "taker", "production"
     )
-    # makers = api.get_market_makers(1)
-    # print(makers)
-    # levels = api.get_price_levels(1, ["mm4", "mm5"])
-    # print(levels)
+    makers = api.get_market_makers(1)
+    print(makers)
+    levels = api.get_price_levels(1, ["mm4", "mm5"])
+    print(levels)
     wallet = os.environ["HASHFLOW_TEST_WALLET"]
     quote = api.request_quote(chain_id=1,
                               base_token='0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
