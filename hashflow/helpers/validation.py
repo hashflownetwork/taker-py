329eb0190a1596b1e5e293d407cb93cdbd0e7632
Sameer Brenn <sameer@hashflow.com>
2023-05-21 20:50:56 -0600

    Fix mixed-case evm address verification

--- hashflow/helpers/validation.py
+++ hashflow/helpers/validation.py
@@ -9,12 +9,8 @@ def validate_chain_id(chain_id):
 
 
 def validate_evm_address(evm_address):
-    case_insensitive = re.compile("^(0x)?[0-9a-f]{40}$", re.IGNORECASE)
-    all_caps = re.compile("^(0x)?[0-9A-F]{40}$")
-    lower_case = re.compile("^(0x)?[0-9a-f]{40}$")
-    if not case_insensitive.match(evm_address) or (
-        not all_caps.match(evm_address) and not lower_case.match(evm_address)
-    ):
+    case_insensitive = re.compile("^(0x)?[0-9A-Fa-f]{40}$",)
+    if not case_insensitive.match(evm_address):
         raise InvalidUsage(f"Invalid EVM address {evm_address}")
 
 
